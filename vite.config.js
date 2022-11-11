51ee86e6c4918933742af5ea30b4b86aa4e9ccf1
LONG\zhongyuanyou <d>
2022-11-11 16:46:15 +0800

    保存环境配置

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import path from 'path'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({ command, mode }) => {
+  const config = loadEnv(mode, './')
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'), // @代替src
+      },
+    },
+    // 打包配置
+    build: {
+      target: 'modules',
+      outDir: 'dist', //指定输出路径
+      assetsDir: 'static', // 指定生成静态资源的存放路径
+    },
+    // 本地运行配置，及反向代理配置
+    server: {
+      cors: true, // 默认启用并允许任何源
+      open: true, // 在服务器启动时自动在浏览器中打开应用程序
+      proxy: {
+        '/api': {
+          target: config.VITE_BASIC_API,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
