3fa753f72088885e8447f10218db74ca011c5311
LONG\zhongyuanyou <d>
2022-12-05 17:53:27 +0800

    保存

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
